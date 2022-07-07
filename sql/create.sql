96b116bd47ca33d74fb8785891a616db62eab9ac
Saba-SH <sshos20@freeuni.edu.ge>
2022-07-07 15:41:53 +0400

    update SQL commands

---
+++ sql/create.sql
@@ -0,0 +1,45 @@
+CREATE TABLE IF NOT EXISTS users (
+    id bigint PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(128) NOT NULL,
+    password_hash VARCHAR(128) NOT NULL,
+    email VARCHAR(256),
+    is_admin BOOLEAN,
+    first_name VARCHAR(128) NOT NULL,
+    last_name VARCHAR(128)
+);
+
+CREATE TABLE IF NOT EXISTS friendships (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    first_user_id BIGINT,
+    second_user_id BIGINT,
+    FOREIGN KEY (first_user_id) REFERENCES users(id),
+    FOREIGN KEY (second_user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS friend_requests (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    sender_id BIGINT,
+    receiver_id BIGINT,
+    send_date DATETIME,
+    FOREIGN KEY (sender_id) REFERENCES users(id),
+    FOREIGN KEY (receiver_id) REFERENCES users(id)
+);
+
+-- This one needs completion
+CREATE TABLE IF NOT EXISTS quizzes (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    creator_id BIGINT,
+    creation_time DATETIME,
+    FOREIGN KEY (creator_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS challenges (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    sender_id BIGINT,
+    receiver_id BIGINT,
+    quiz_id BIGINT,
+    send_date DATETIME,
+    FOREIGN KEY (sender_id) REFERENCES users(id),
+    FOREIGN KEY (receiver_id) REFERENCES users(id),
+    FOREIGN KEY (quiz_id) REFERENCES quizzes(id)
+);
