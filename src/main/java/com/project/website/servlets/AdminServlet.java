d302276fef028349c360f551c1e4fa327cb6707a
gkldi20 <gkldi20@freeuni.edu.ge>
2022-09-10 11:55:48 +0400

    Added Categories and Announcements management in adminmenu

--- src/main/java/com/project/website/servlets/AdminServlet.java
+++ src/main/java/com/project/website/servlets/AdminServlet.java
@@ -1,6 +1,8 @@
 package com.project.website.servlets;
 
+import com.project.website.DAOs.AnnouncementDAO;
 import com.project.website.DAOs.UserDAO;
+import com.project.website.Objects.Announcement;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -24,6 +26,17 @@ public class AdminServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (!(boolean) request.getSession().getAttribute("admin")) {
+            response.sendRedirect("home");
+            return;
+        }
 
+        AnnouncementDAO announcementDAO = (AnnouncementDAO) request.getServletContext().getAttribute(AnnouncementDAO.ATTR_NAME);
+        if (request.getParameter("announcement-title") != null) {
+            String announcementTitle = request.getParameter("announcement-title");
+            String announcementText = request.getParameter("announcement-text");
+            announcementDAO.insertAnnouncement(new Announcement(((Long)request.getSession().getAttribute("userID")).intValue(), announcementTitle, announcementText));
+        }
+        request.getRequestDispatcher("WEB-INF/admin.jsp").forward(request, response);
     }
 }
